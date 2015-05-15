33563b8a85e34004af8543cac2b1a13c08ca857f
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-05-15 23:41:55 +0100

    Separate gem specification

---
+++ config.rb
@@ -0,0 +1,22 @@
+require 'rake'
+
+PKG_NAME = 'hornetseye-kinect'
+PKG_VERSION = '1.1.2'
+CFG = RbConfig::CONFIG
+CXX = ENV[ 'CXX' ] || 'g++'
+RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
+CC_FILES = FileList[ 'ext/*.cc' ]
+HH_FILES = FileList[ 'ext/*.hh' ] + FileList[ 'ext/*.tcc' ]
+TC_FILES = FileList[ 'test/tc_*.rb' ]
+TS_FILES = FileList[ 'test/ts_*.rb' ]
+SO_FILE = "ext/#{PKG_NAME.tr '\-', '_'}.#{CFG[ 'DLEXT' ]}"
+PKG_FILES = [ 'Rakefile', 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + CC_FILES + HH_FILES + TS_FILES + TC_FILES
+BIN_FILES = [ 'README.md', 'COPYING', '.document', SO_FILE ] +
+            RB_FILES + TS_FILES + TC_FILES
+SUMMARY = %q{Acquisition of images and depth maps using libfreenect}
+DESCRIPTION = %q{This Ruby extension provides an interface to the Microsoft Kinect}
+LICENSE = 'GPL-3+'
+AUTHOR = %q{Jan Wedekind}
+EMAIL = %q{jan@wedesoft.de}
+HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-kinect/}
